59da6e3702fe5637f852eaca11c2f159eca93f78
Mushmat <edu.chirayu2005@gmail.com>
2024-06-26 22:51:39 +0530

    Setup the CategoryDetailScreen.kt so that description of each category can be displayed.

--- app/src/main/java/com/example/myrecipeapp/Category.kt
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -1,8 +1,10 @@
 package com.example.myrecipeapp
 
-data class Category(val idCategory:String,
+data class Category(
+    val idCategory:String,
     val strCategory:String,
     val strCategoryThumb:String,
-    val strCategoryDescription:String)
+    val strCategoryDescription:String
+)
 
 data class CategoriesResponse(val categories: List<Category>) //The 'categories' is a list in TheMealDB which has the list of all the objects.
---
+++ app/src/main/java/com/example/myrecipeapp/CategoryDetailScreen.kt
@@ -0,0 +1,41 @@
+package com.example.myrecipeapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+
+@Composable
+fun CategoryDetailScreen(category: Category){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = category.strCategory, textAlign = TextAlign.Center)
+        Image(
+            painter = rememberAsyncImagePainter(category.strCategoryThumb), //This is the content of the image
+            contentDescription = "${category.strCategory} Thumbnail",
+            modifier = Modifier
+                .wrapContentSize()
+                .aspectRatio(1f) //Just be as wide as it is high.
+        )
+        Text(text = category.strCategoryDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.verticalScroll(rememberScrollState()) //Allows us to scroll through the text is the text is too long to fit in a single dimensional screen
+
+        )
+
+    }
+}
