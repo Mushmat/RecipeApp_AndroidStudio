361125f060f010f4402546aca8100e7eec530364
Mushmat <edu.chirayu2005@gmail.com>
2024-06-26 13:58:16 +0530

    Setting up the data class for different categories of food.

---
+++ app/src/main/java/com/example/myrecipeapp/Category.kt
@@ -0,0 +1,8 @@
+package com.example.myrecipeapp
+
+data class Category(val idCategory:String,
+    val strCategory:String,
+    val strCategoryThumb:String,
+    val strCategoryDescription:String)
+
+data class CategoriesResponse(val categories: List<Category>) //The 'categories' is a list in TheMealDB which has the list of all the objects.
