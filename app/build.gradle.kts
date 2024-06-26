96a1dafc179fcb07276f77bcf47b1adae475fb32
Mushmat <edu.chirayu2005@gmail.com>
2024-06-26 17:31:13 +0530

    Basic Version Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
     //Compose ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     //Network calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0") //Its an external party that has build the code that will be use. This here is a network call
 
@@ -62,7 +62,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
